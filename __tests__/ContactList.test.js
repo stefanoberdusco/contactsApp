ef34ec5bbbfb8356680e0ae99d35fd9cced74646
Berdusco <sberdusco@l203402.ni.ad.newsint>
2020-04-05 14:02:14 +0100

    feat: setup tests

--- __tests__/ContactList.test.js
+++ __tests__/ContactList.test.js
@@ -4,11 +4,12 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import {ContactList} from '../src/components/ContactList';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const tree = renderer.create(<ContactList />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
